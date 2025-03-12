31b7d5f5d468c14506fab791f0aa654244e1e5c5
faisal-ft2205767 <ft2205767@qu.edu.qa>
2025-03-12 14:23:48 +0300

    login use case

---
+++ Web Project/js/login.js
@@ -0,0 +1,35 @@
+let email = [];
+let password = [];
+let state = [];
+
+fetch("../data/email.json")
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(element => {
+            email.push(element.email);
+            password.push(element.password);
+            state.push(element.state);
+        });
+        
+    })
+
+const valueEmail = document.querySelector("#e-mail");
+const valuePassword = document.querySelector("#passwordd");
+const submit = document.querySelector("#login");
+
+
+submit.addEventListener("submit", login)
+
+async function login(e){
+    e.preventDefault();
+    inputEmail = valueEmail.value;
+    inputPassword = valuePassword.value;
+
+    email.forEach((element, index) =>{
+        if(element.toLowerCase() === inputEmail.toLowerCase() && password[index] === inputPassword){
+            window.location.href = `../html/${state[index]}.html`;
+        } 
+    })
+    
+    
+}
---
+++ Web Project/js/student.js
@@ -0,0 +1,8 @@
+fetch("../data/courses.json")
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(element => {
+        
+        });
+        
+    })
