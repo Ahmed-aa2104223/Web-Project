316ebe7d5f415d450f604cfc9ae4111c525a2b6d
faisal-ft2205767 <ft2205767@qu.edu.qa>
2025-03-12 23:49:11 +0300

    displayed all courses

--- Web Project/js/student.js
+++ Web Project/js/student.js
@@ -1,8 +1,25 @@
-fetch("../data/courses.json")
-    .then(res => res.json())
-    .then(data => {
-        data.forEach(element => {
-        
-        });
-        
-    })
+const courses = document.querySelector("#courses");
+
+read();
+
+function renderCourses(data){
+    if(!data.course_name == ""){
+        return ` <div class="card">
+                    <h2>${data.course_code}</h2>
+                    <p>${data.course_name}</p>
+                    <p>${data.credit_hour} credits</p>
+                    <p>No prerequiste needed</p>
+                </div>`;
+    }
+
+}
+
+
+async function read(){
+    const response = await fetch('../data/courses.json');
+    const data = await response.json();
+    data.forEach(element => {
+        courses.innerHTML += renderCourses(element);
+    });
+
+}
